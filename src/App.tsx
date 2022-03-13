b9e85d3b1d225ef4fdbfdf6de9b6f8e0ca585828
Andrew Fleming <andrewpling@gmail.com>
2022-03-13 16:26:40 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//Components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from "@material-ui/core/Badge";
+import Item from './Item/Item'
+
+//styles
+import { Wrapper } from './App.styles';
+
+//types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+  //All fetching is done inside async functions
+  //We put the .json at the end because that is what APIs generally send back
+
+  //A promise is what we get back when fetching data
+  //In this case, the data we want to get back from the API is the CartItemType type
+  //The data we receive back is an array of cart items
+
+
+const App = () =>{
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products', 
+    getProducts
   );
+  console.log(data); //returns the full array of items from the API
+
+    const getTotalItems = () => null;
+    
+    const handleAddToCart = (clickedItem: CartItemType) => null;
+
+    const handleRemoveFromCart = () => null;
+
+    if(isLoading) return <LinearProgress />;
+    //displays the material ui core loading bar at the top while waiting for the data to come in
+
+    if(error) return <div>Something went wrong</div>;
+
+    return (
+      <Wrapper>
+        <Grid container spacing={3}>
+          {data?.map(item => (
+            <Grid item key={item.id} xs={12} sm={4}>
+              <Item item={item} handleAddToCart={handleAddToCart} />
+            </Grid>
+          ))}
+        </Grid>
+      </Wrapper>
+    )
+
+
+  return <div className="App">Start</div>;
 }
 
 export default App;
