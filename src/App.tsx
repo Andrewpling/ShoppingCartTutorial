9febc9459f4fc871f72d9643d826157010be0f6f
Andrew Fleming <andrewpling@gmail.com>
2022-03-13 16:58:57 -0400

    feat: added some cart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "react-query";
 
 //Components
 import Drawer from "@material-ui/core/Drawer";
+import Cart from './Cart/Cart';
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
@@ -10,7 +11,7 @@ import Badge from "@material-ui/core/Badge";
 import Item from './Item/Item'
 
 //styles
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton } from './App.styles';
 
 //types
 export type CartItemType = {
@@ -35,13 +36,16 @@ const getProducts = async (): Promise<CartItemType[]> =>
 
 
 const App = () =>{
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products', 
     getProducts
   );
   console.log(data); //returns the full array of items from the API
 
-    const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => 
+    items.reduce((ack: number, item) => ack + item.amount, 0);  
     
     const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -54,6 +58,18 @@ const App = () =>{
 
     return (
       <Wrapper>
+        <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+          <Cart 
+            cartItems={cartItems} 
+            addToCart={handleAddToCart}
+            removeFromCart={handleRemoveFromCart}
+          />
+        </Drawer>
+        <StyledButton onClick={() => setCartOpen(true)}>
+          <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+            <AddShoppingCartIcon />
+          </Badge>
+        </StyledButton>
         <Grid container spacing={3}>
           {data?.map(item => (
             <Grid item key={item.id} xs={12} sm={4}>
